8b0b77506e7fe27f552b6cd1b783bd2896c3d98d
dbarry9 <dbarry@vols.utk.edu>
2018-10-15 02:31:04 +0000

    Updated the script to read-in the lists. Still need to store in mongodb.

--- dbarry-lite.py
+++ dbarry-lite.py
@@ -3,7 +3,6 @@ import re
 import pymongo
 import json
 import time
-import datetime
 import requests
 
 dbname = "fdac18mp2" #please use this database
@@ -14,81 +13,34 @@ client = pymongo.MongoClient()
 db = client[dbname]
 coll = db[collname]
 
+# Start search on the first page. No results yet.
 pgnum = 0
-beginurl = "https://gitlab.com/search?page=" + str(pgnum) + "&scope=&search=" + word + "&snippets="
+numfound = 0
 
-for i in 
+## Read-in the keywords.
+#f = open("dbarry_keywords.txt", 'r')
+#lines = f.readlines()
+#f.close()
 
+#beginurl = "https://gitlab.com/search?page=" + str(pgnum) + "&scope=&search=" + word + "&snippets="
+#while numfound < 50:
+#	for line in lines:
+#		print(line)
 
-# check remaining query chances for rate-limit restriction
-def wait(left):
-    global header
-    while (left < 20):
-        l = requests.get('https://gitlab.com/api/v4/projects', headers=header)
-        if (l.ok):
-            left = int(l.headers.get('RateLimit-Remaining'))
-        time .sleep(60)
-    return left
+## Read-in the URLs from the SourceForge list.
+f = open("dbarry_sflist.txt", 'r')
+lines = f.readlines()
+f.close()
 
-# send queries and extract urls 
-def get(url, coll):
+## Store the URLs from the SourceForge List.
+for line in lines:
+	print(line)
 
-    global gleft
-    global header
-    global bginnum
-    gleft = wait(gleft)
-    values = []
-    size = 0
+## Read-in the URLs from the GitLab list.
+f = open("dbarry_gllist.txt", 'r')
+lines = f.readlines()
+f.close()
 
-    try:
-        r = requests .get(url, headers=header)
-        time .sleep(0.5)
-        # got blocked
-        if r.status_code == 403:
-            return "got blocked", str(bginnum)
-        if (r.ok):
-
-            gleft = int(r.headers.get('RateLimit-Remaining'))
-            lll = r.headers.get('Link')
-            t = r.text
-            array = json.loads(t)
-            
-            for el in array:
-                coll.insert(el)
- 
-            #next page
-            while ('; rel="next"' in lll):
-                gleft = int(r.headers.get('RateLimit-Remaining'))
-                gleft = wait(gleft)
-                # extract next page url
-                ll = lll.replace(';', ',').split(',')
-                url = ll[ll.index(' rel="next"') -
-                         1].replace('<', '').replace('>', '').lstrip()
-             
-                try:
-                    r = requests .get(url, headers=header)
-                    if r.status_code == 403:
-                        return "got blocked", str(bginnum)
-                    if (r.ok):
-                        lll = r.headers.get('Link')
-                        t = r.text
-                        array1 = json.loads(t)
-                        for el in array1:
-                            coll.insert(el)
-                    else:
-                        sys.stderr.write("url can not found:\n" + url + '\n')
-                        return 
-                except requests.exceptions.ConnectionError:
-                    sys.stderr.write('could not get ' + url + '\n')
-
-        else:
-            sys.stderr.write("url can not found:\n" + url + '\n')
-            return
-
-    except requests.exceptions.ConnectionError:
-        sys.stderr.write('could not get ' + url + '\n')
-    except Exception as e:
-        sys.stderr.write(url + ';' + str(e) + '\n')
-        
-#start retrieving        
-get(beginurl,coll)
+## Store the URLs from the GitLab List.
+for line in lines:
+	print(line)
