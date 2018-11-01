993bac8124d05e60f96e22ad06f4883c8f7d4a21
hossain-rayhan <hossain.rayhan@outlook.com>
2018-11-01 23:07:39 +0000

    Miniproject2 Phase2: Step 1 - Spet 4 Done

--- extrRels.py
+++ extrRels.py
@@ -1,7 +1,7 @@
 import pymongo, json, sys
 client = pymongo.MongoClient (host="da1")
 db = client ['fdac18mp2']
-id = "audris"
+id = "rhossai2"
 coll = db [ 'releases_' + id]
 for r in coll.find():
   n = r['name']
